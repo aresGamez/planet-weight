55dafe0ec4c69c5edbaaf51dfd5196e289b0b013
aresGamez <86250908+aresGamez@users.noreply.github.com>
2021-06-21 12:18:28 -0400

    Add files via upload

---
+++ PlanetWeight.cpp
@@ -0,0 +1,38 @@
+
+#include <iostream>
+// This code is made by AresGamez
+using namespace std;
+int weight;
+int moon;
+int venus;
+int mercury;
+string planet;
+int main()
+{
+    cout << "Welcome to the Planet Weight Calculator\n";
+    cout << "Please pick a planet(Moon, Venus)\n";
+    cin >> planet;
+
+    if (planet == "Moon" || planet == "moon")
+    {
+        cout << "Please enter a weight(lbs)\n";
+        cin >> weight;
+        moon = weight * 0.165;
+        cout << "Your weight on the Moon would be: " << moon << " lbs";
+    }
+    if (planet == "Venus" || planet == "venus")
+    {
+        cout << "Please enter a weight(lbs)\n";
+        cin >> weight;
+        venus = weight * 0.9042;
+        cout << "Your weight on Venus would be: " << venus << " lbs";
+    }
+    if (planet == "Mercury" || planet == "mercury")
+    {
+        cout << "Please enter a weight(lbs)\n";
+        cin >> weight;
+        mercury = weight * 0.3772;
+        cout << "Your weight on Mercury would be " << mercury << " lbs";
+    }
+}
+
